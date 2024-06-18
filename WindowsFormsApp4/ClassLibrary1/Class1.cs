ea80ad0bfd11b23818e07bae3e49f5f02317b0de
Pablocesarc <118829360+Pablocesarc@users.noreply.github.com>
2024-06-18 08:41:49 -0400

    Add files via upload

---
+++ WindowsFormsApp4/ClassLibrary1/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        String con = "Data Source = PABLITOJUNIOR\\THEGAMMER;Initial Catalog = estudiante; Integrated Security = True";
+        public void insertar(string a, string b, string c)
+        {
+            using (SqlConnection cn = new SqlConnection(con))
+            {
+                SqlCommand cmd = new SqlCommand("insert into Estudiantes(nombre, apellido, edad) values ('" + a + "','" + b + "'," + Convert.ToInt32(c) + ")", cn);
+                cmd.CommandType = CommandType.Text;
+                cn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+        public DataTable mostrar()
+        {
+            DataTable dt = new DataTable();
+            using (SqlConnection cn = new SqlConnection(con))
+            {
+                SqlDataAdapter dap = new SqlDataAdapter("select * from Estudiantes", cn);
+                dap.SelectCommand.CommandType = CommandType.Text;
+                cn.Open();
+                dap.Fill(dt);
+            }
+            return dt;
+        }
+    }
+}
---
+++ WindowsFormsApp4/WindowsFormsApp4/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        ClassLibrary1.Class1 cl = new ClassLibrary1.Class1();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            cl.insertar(textBox2.Text, textBox3.Text, textBox4.Text);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DataTable dt2 = new DataTable();
+            dt2 = cl.mostrar();
+            dataGridView1.DataSource = dt2;
+        }
+
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
